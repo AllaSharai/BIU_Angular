5a8604dd00f39e75212c6e95b002acce206ad86c
Alla Chebotarska <alla.chebotarska@gmail.com>
2018-12-14 18:36:54 +0100

    add video at home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,60 @@
 var appModule = angular.module('SomeApp', ['ui.bootstrap']);
 appModule.controller("myCtrl", function ($scope) {
+
     $scope.records = [
         {
-            title: "title1",
-            desc: "gjjk ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Austria",
+            desc: "This photo was taken somewhere in Austria",
             img: "img/image1.jpg"
         },
         {
-            title: "title2",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Phuket",
+            desc: "This photo was taken somewhere in Phuket",
             img: "img/image2.jpg"
         },
         {
-            title: "title3",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Spain",
+            desc: "This photo was taken somewhere in Spain",
             img: "img/image3.jpg"
         },
         {
-            title: "title4",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Alaska",
+            desc: "This photo was taken somewhere in Spain",
             img: "img/image4.jpg"
         },
         {
-            title: "title5",
-            desc: "Lorem ipsum dolor sit1 amet, consectetur adipiscing elit.",
+            title: "Tenerife",
+            desc: "This photo was taken somewhere in Tenerife",
             img: "img/image5.jpg"
         },
         {
-            title: "title6",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Toronto",
+            desc: "This photo was taken somewhere in Toronto",
             img: "img/image6.jpg"
         },
         {
-            title: "title7",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Hong Kong",
+            desc: "This photo was taken somewhere in Hong Kong",
             img: "img/image7.jpg"
         },
         {
-            title: "title8",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Italy",
+            desc: "This photo was taken somewhere in Italy",
             img: "img/image8.jpg"
         },
         {
-            title: "title9",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            title: "Norway",
+            desc: "This photo was taken somewhere in Norway",
             img: "img/image9.jpg"
         },
     ];
     $scope.images = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    $scope.showMe = false;
+    $scope.buttonText = "Read more";
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
+        $scope.buttonText = $scope.showMe ? "Hide" : "Read more"}
+
+
 });
